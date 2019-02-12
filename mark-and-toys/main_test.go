e66b9c464a78b7e5e635441e27ca63240c512b62
Emmanuel Goh <emmanuel.goh.7@gmail.com>
2019-02-13 00:20:58 +0800

    mark and toys
    Signed-off-by: Emmanuel Goh <emmanuel.goh.7@gmail.com>

---
+++ mark-and-toys/main_test.go
@@ -0,0 +1,24 @@
+package main
+
+import "testing"
+
+func Test_maximumToys(t *testing.T) {
+	type args struct {
+		prices []int32
+		k      int32
+	}
+	tests := []struct {
+		name string
+		args args
+		want int32
+	}{
+		{args: args{prices: []int32{1, 12, 5, 111, 200, 1000, 10}, k: 50}, want: 4},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := maximumToys(tt.args.prices, tt.args.k); got != tt.want {
+				t.Errorf("maximumToys() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
